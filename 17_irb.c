a42eabb5c0b41ba3b7ff7a5682f742f9757d3b2d
Helekson <danilos2005.com@gmail.com>
2024-12-27 03:25:19 +0300

    17_irb.c

--- 17_irb.c
+++ 17_irb.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-char *str_cat(char * str1, char const * str2) {
+char *str_cat(const char *str1, const char *str2) {
+    size_t ln1 = strlen(str1);
+    size_t ln2 = strlen(str2);
 
-    int ln1 = strlen(str1);
-    int ln2 = strlen(str2);
-
-    for(int i = 0; i <= ln2; i++) {
-
-        str1[ln1 + i] = str2[i];
+    char *result = malloc(ln1 + ln2 + 1);
+    if (result == NULL) {
+        printf("Error: Memory allocation failed.\n");
+        return NULL;
     }
 
-    return str1;
+    for (size_t i = 0; i < ln1; i++) {
+        result[i] = str1[i];
+    }
+
+    for (size_t i = 0; i <= ln2; i++) {
+        result[ln1 + i] = str2[i];
+    }
+
+    return result;
 }
 
 int main() {
+    const char str1[] = "Hello, ";
+    const char str2[] = "world!";
 
-    char str1[100] = "Hello, ";
-    char str2[] = "world!";
+    char *result = str_cat(str1, str2);
+    if (result != NULL) {
+        printf("%s\n", result);
 
-    str_cat(str1, str2);
-
-    printf("%s", str1);
+        free(result);
+    }
 
     return 0;
 }
